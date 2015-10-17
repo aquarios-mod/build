8fa2e19abe4034dd13d5a6f9757efba252d86d9f
beanstown106 <nbenis106@gmail.com>
2015-10-17 06:16:04 -0400

    Drop deprecated stuff/move stuff to vendor/device (squashed)
    *drop aosp messaging in favor of google messenger
    *drop deskclock in favor of googles
    *drop email and exchange in favor of googles
    *drop camera2 in favor of googles camera
    *drop deprecated aosp gallery2
    *drop deprecated aosp calendar
    *drop deprecated launcher2
    *drop deprecated music
    *drop deprecated picotts
    *drop deprecated quicksearchbox
    *drop deprecated sample home
    *drop deprecated openwnn keyboard
    *drop deprecated protips
    *drop deprecated video editor libraries
    *drop launcher 3 we will set in vendor
    *drop outdated wallpapers we will set livewallpaper picker in vendor
    *drop keyguard.no_require_sim=true we will set in vendor
    *drop dataroaming=true we will set this in vendor
    *drop apns-conf we will set this per device
    
    Change-Id: I907da38d3ededb1bf1d81c0371d90d00750b2edf

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -21,27 +21,10 @@
 
 PRODUCT_PACKAGES := \
     libfwdlockengine \
-    OpenWnn \
-    libWnnEngDic \
-    libWnnJpnDic \
-    libwnndict \
     WAPPushManager
-
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    PhotoTable
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
-
-# Get the TTS language packs
-$(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/locales_full.mk)
