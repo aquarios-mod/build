436d40593746ad3e295e3ba51e57496c14700fa7
calebcabob <calphonic@gmail.com>
2018-01-14 01:21:37 -0500

    Adjust path in irdervg
    Change-Id: I2fc7715bcbddf9405fb169d86bc0c61e4f248f92
    Signed-off-by: calebcabob <calphonic@gmail.com>

--- core/product_config.mk
+++ core/product_config.mk
@@ -170,7 +170,7 @@ include $(BUILD_SYSTEM)/device.mk
 
 # An AquariOS build needs only the AquariOS product makefiles.
 ifneq ($(CUSTOM_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(CUSTOM_BUILD)/aquarios.mk")
+  all_product_configs := $(shell ls vendor/aquarios/products/$(CUSTOM_BUILD).mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
